65197c8813a1f8eac14a7238d9c91e7929791fb9
tatsu <tatuo0228@icloud.com>
2018-07-04 10:32:43 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by たつ on 2018/07/04.
+//  Copyright © 2018年 tatsuo sugimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
